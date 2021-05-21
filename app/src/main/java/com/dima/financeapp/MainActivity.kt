c31fbcbf0250b29bab8c2d7c40b00b87026b1f08
Dmitry Turov <dmitry.turov374@gmail.com>
2021-05-21 21:25:45 +0300

    first commit

---
+++ app/src/main/java/com/dima/financeapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dima.financeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
