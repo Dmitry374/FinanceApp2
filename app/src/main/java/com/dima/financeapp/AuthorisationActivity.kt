545ab4be03bf0d1559717f2d47439b83c28f55d7
Dmitry Turov <dmitry.turov374@gmail.com>
2021-05-22 01:38:37 +0300

    добавлен пустой фрагмент авторизации

--- app/src/main/java/com/dima/financeapp/AuthorisationActivity.kt
+++ app/src/main/java/com/dima/financeapp/AuthorisationActivity.kt
@@ -4,8 +4,15 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class AuthorisationActivity : AppCompatActivity() {
+
+    private var authorisationFragment = AuthorisationFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authorisation)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.nav_host_authorisation_container, authorisationFragment)
+            .commit()
     }
 }
