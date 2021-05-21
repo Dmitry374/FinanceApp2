667232eb47b970ec74a46c73da5eefa919a5886d
Dmitry Turov <dmitry.turov374@gmail.com>
2021-05-22 01:19:40 +0300

    добавлен пустой кран авторизации

---
+++ app/src/main/java/com/dima/financeapp/AuthorisationActivity.kt
@@ -0,0 +1,11 @@
+package com.dima.financeapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AuthorisationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authorisation)
+    }
+}
--- app/src/main/java/com/dima/financeapp/StartActivity.kt
+++ app/src/main/java/com/dima/financeapp/StartActivity.kt
@@ -1,12 +1,31 @@
 package com.dima.financeapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 
 class StartActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
         window.setBackgroundDrawableResource(R.drawable.background)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            navigateToAuthorisationScreen()
+        }, DELAY_IM_MILLIS)
+    }
+
+    private fun navigateToAuthorisationScreen() {
+        val intent = Intent(this, AuthorisationActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(intent)
+        finish()
+    }
+
+    companion object {
+        private const val DELAY_IM_MILLIS = 1000L
     }
 }
