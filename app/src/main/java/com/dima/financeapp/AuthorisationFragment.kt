2f44b30e0cccaf282590c638a035164e0acae6ff
Dmitry Turov <dmitry.turov374@gmail.com>
2021-05-22 01:57:14 +0300

    фрагмент авторизации

--- app/src/main/java/com/dima/financeapp/AuthorisationFragment.kt
+++ app/src/main/java/com/dima/financeapp/AuthorisationFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.fragment_authorisation.*
 
 class AuthorisationFragment : Fragment() {
 
@@ -12,5 +14,13 @@ class AuthorisationFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         return inflater.inflate(R.layout.fragment_authorisation, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Glide.with(imgLogoAuthorisation)
+            .load(R.drawable.logo)
+            .into(imgLogoAuthorisation)
     }
 }
