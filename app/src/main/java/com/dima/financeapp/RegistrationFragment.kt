81a782ee59ae63c1ab5160be195faddefb0afea2
Dmitry Turov <dmitry.turov374@gmail.com>
2021-05-22 03:05:25 +0300

    фрагмент регистрации

--- app/src/main/java/com/dima/financeapp/RegistrationFragment.kt
+++ app/src/main/java/com/dima/financeapp/RegistrationFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.fragment_registration.*
 
 class RegistrationFragment : Fragment() {
 
@@ -12,5 +14,13 @@ class RegistrationFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         return inflater.inflate(R.layout.fragment_registration, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Glide.with(imgLogoRegistration)
+            .load(R.drawable.logo)
+            .into(imgLogoRegistration)
     }
 }
